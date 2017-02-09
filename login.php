c70f751bd65eb17df11cfaab8a69ddc45d0e26a9
Rob Cleaton <rob.cleaton@digital.homeoffice.gov.uk>
2017-02-09 17:53:29 +0000

    login vertically center

--- login.php
+++ login.php
@@ -5,17 +5,37 @@ $pageTitle = "Login";
 <style>
   body {
     background: rgba(255, 215, 58, 0.71);
+    margin: 0px;
+    padding: 0px;
   }
   .navbar, footer {
     display: none;
   }
+  h1 {
+    display: block;
+  }
+
+      .container {
+        display: table;
+        position: absolute;
+        height: 100%;
+        width: 100%;
+      }
+
+      .vertical-center {
+        display: table-cell;
+        vertical-align: middle;
+      }
+
+
 </style>
 
   <div class="container">
 
-    <div class="row">
+    <div class="row vertical-center">
       <div class="col-sm-6 col-sm-offset-3">
-        <h1>OATBook</h1>
+        <h1 class="text-center">OATBook</h1>
+
         <form action="/">
           <div class="form-group">
             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
