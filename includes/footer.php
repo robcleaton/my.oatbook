dab4d2c810ff164e47ce8520be615413a1560cbf
Rob Cleaton <rob.cleaton@digital.homeoffice.gov.uk>
2017-02-13 11:06:01 +0000

    styled caret and added analytics

--- includes/footer.php
+++ includes/footer.php
@@ -24,6 +24,7 @@
     </div><!-- /row -->
   </footer><!-- /container-fluid -->
 
+  <?php include'analytics.php'; ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
